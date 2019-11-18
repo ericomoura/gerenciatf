5de42d49f588420940dd29d495f8dea0391bf2b2
Erico Moura <ericomoura96@gmail.com>
2019-11-18 19:56:35 -0300

    gcc and ossec

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
 apt-get update
-apt-get install iptools-ping
+apt-get install iputils-ping
+apt-get install gcc
+
+wget https://github.com/ossec/ossec-hids/archive/3.1.0.tar.gz -P /tmp
+cd /tmp
+tar xzf 3.1.0.tar.gz
+cd ossec-hids-3.1.0
+./install.sh
