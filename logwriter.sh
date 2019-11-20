41984293b5784c07ef751670e46933fcd3248be9
Your Name <you@example.com>
2019-11-20 00:23:10 -0300

    d

--- logwriter.sh
+++ logwriter.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-if [ $1 -eq 1 ]
+if [ $1 -eq 0 ]
+then
+    echo '' > /home/testlog.log
+
+elif [ $1 -eq 1 ]
 then
     echo '2013-11-01T10:01:04.600374-04:00 arrakis ossec-exampled[9123]: test connection from 192.168.1.1 via test-protocol1' >> /home/testlog.log
 
