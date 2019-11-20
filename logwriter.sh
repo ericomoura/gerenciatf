400956bfa9bc4281b87561885aafa3000e4e045a
Your Name <you@example.com>
2019-11-20 00:19:50 -0300

    c

--- logwriter.sh
+++ logwriter.sh
@@ -12,6 +12,10 @@ elif [ $1 -eq 3 ]
 then
     echo 'Jun 30 23:58:38 debian sshd[13444]: Failed password for root from 112.85.42.146 port 56969 ssh2' >> /home/testlog.log
 
+elif [ $1 -eq 4 ]
+then
+    echo 'Jan 21 02:39:54 linux-agent sshd[30449]: Invalid user blimey from 208.103.56.41 port 51498' >> /home/testlog.log
+
 else
     echo Invalid argument
 fi
